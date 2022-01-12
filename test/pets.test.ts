84c3a9886cdd8bbdde83d0a95c502f16965bd510
Oleh <olegsimfi@gmail.com>
2022-01-12 22:49:13 +0200

    Add Request Builder

--- test/pets.test.ts
+++ test/pets.test.ts
@@ -1,9 +1,9 @@
-import { strict as assert } from 'assert'
+import {deepEqual, strict as assert} from 'assert'
 import {PetController} from "../api/controller/pet.controller";
 
 const pet = new PetController();
 
-describe('Pets', () => {
+describe('Pet', () => {
     it('can get pet by id', async function () {
         const body = await pet.getById(1);
         assert(body.id == 1, `Expect API to return pet with id 1 but got ${body.id}`);
@@ -29,13 +29,64 @@ describe('Pets', () => {
     });
 
     it('can get pet by tag', async function () {
-
         const body = await pet.findByTags('tag1')
         assert(body.length > 0)
-        assert(body.some(
+        assert(body.every(
             (pet: any) => pet.tags.some(
                 (tag: any) => tag.name == 'tag1')
             )
         )
     })
+
+    it('can be added, updated and deleted', async function () {
+        const petToCreate = {
+            "category": {
+                "id": 0,
+                "name": "string"
+            },
+            "name": "Cat",
+            "photoUrls": ["https://test.com/image.jpg"],
+            "tags":[
+                {
+                    "id": 0,
+                    "name": "string"
+                },
+            ],
+            "status": "available"
+        }
+        const addedPet = await pet.addNew(petToCreate)
+
+        assert.deepEqual(addedPet, {
+            ...petToCreate,
+            id: addedPet.id
+        },`Expected created pet to match data used upon creation`)
+
+        const foundAddedPet = await pet.getById(addedPet.id)
+        assert.deepEqual(foundAddedPet,
+            {...petToCreate,
+            id: addedPet.id},
+            `Expected created pet to match data used upon creation`)
+
+        const newPet = {
+            id: addedPet.id,
+            category: {
+                "id": 1,
+                "name": "string2"
+            },
+            "name": "Dog",
+            "photoUrls": ["https://test.com/image2.jpg"],
+            "tags":[
+                {
+                    "id": 1,
+                    "name": "string2"
+                },
+            ],
+            "status": "pending"
+        }
+        const updatedPet = await pet.update(newPet)
+        assert.deepEqual(updatedPet, newPet, `Expected updated pet to equal data used upon updating`)
+        await pet.delete(addedPet.id)
+
+        //TODO: assert 404 error on attemp to get pet thet was deleted
+    });
 }) 
