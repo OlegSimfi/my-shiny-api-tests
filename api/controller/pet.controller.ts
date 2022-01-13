3c609d20bb7da7e54787f7c07dd030796399f070
Oleh <olegsimfi@gmail.com>
2022-01-13 23:00:11 +0200

    Add Asserting response structure: Writing JSON schema

--- api/controller/pet.controller.ts
+++ api/controller/pet.controller.ts
@@ -1,47 +1,98 @@
-import {URLSearchParams} from "url";
-import {JsonRequest} from 'http-req-builder';
-import {definitions, operations} from "../../.temp/types";
+import { URLSearchParams } from 'url'
+import { JsonRequest } from 'http-req-builder'
+import { definitions, operations } from '../../.temp/types'
+import { validate } from '../validator'
 
-export class  PetController {
-    async getById (id: number | string) {
-        return (await new JsonRequest()
-            .url(`http://localhost/v2/pet/${id}`)
-            .send<operations['getPetById']['responses']['200']['schema']>()
+export class PetController {
+    async getById(id: number | string) {
+        const body = (
+            await new JsonRequest()
+                .url(`http://93.126.97.71:10080/api/pet/${id}`)
+                .send<operations['getPetById']['responses']['200']['schema']>()
+        ).body
+        const schema = {
+            "$schema": "http://json-schema.org/draft-07/schema",
+            "type": "object",
+            "properties": {
+                "id": {
+                    "type": "integer"
+                },
+                "category": {
+                    "type": "object",
+                    "properties": {
+                        "id": {
+                            "type": "integer"
+                        },
+                        "name": {
+                            "type": "string"
+                        }
+                    }
+                },
+                "name": {
+                    "type": "string"
+                },
+                "photoUrls": {
+                    "type": "array",
+                    "items": {
+                        "type": "string"
+                    }
+                },
+                "tags": {
+                    "type": "array",
+                    "items": {
+                        "type": "object",
+                        "properties": {
+                            "id": {
+                                "type": "integer"
+                            },
+                            "name": {
+                                "type": "string"
+                            }
+                        }
+                    }
+                },
+                "status": {
+                    "type": "string"
+                }
+            }
+        }
+
+        validate(schema, body)
+        return body
+    }
+
+    async findByTags(tags: string | string[]) {
+        return (
+            await new JsonRequest()
+                .url('http://93.126.97.71:10080/api/pet/findByTags')
+                .searchParams(new URLSearchParams({ tags }))
+                .send<operations['findPetsByTags']['responses']['200']['schema']>()
         ).body
     }
 
-    async findByStatus ( status: string | string []) {
+    async findByStatus(status: string | string[]) {
         return (
             await new JsonRequest()
-                .url('http://localhost/v2/pet/findByStatus')
+                .url('http://93.126.97.71:10080/api/pet/findByStatus')
                 .searchParams(new URLSearchParams({ status }))
                 .send<operations['findPetsByStatus']['responses']['200']['schema']>()
         ).body
     }
 
-    async findByTags ( tags: string | string []) {
-        return(
-             await new JsonRequest()
-                 .url('http://localhost/v2/pet/findByTags')
-                 .searchParams(new URLSearchParams({ tags }))
-                 .send<operations['findPetsByTags']['responses']['200']['schema']>()
-        ).body
-    }
-
-    async addNew (pet: Omit<definitions['Pet'], 'id'>) {
+    async addNew(pet: Omit<definitions['Pet'], 'id'>) {
         return (
             await new JsonRequest()
-                .url(`http://localhost/v2/pet`)
+                .url(`http://93.126.97.71:10080/api/pet`)
                 .method('POST')
                 .body(pet)
                 .send<operations['addPet']['responses']['200']['schema']>()
         ).body
     }
 
-    async update (pet: definitions['Pet']) {
+    async update(pet: definitions['Pet']) {
         return (
             await new JsonRequest()
-                .url(`http://localhost/v2/pet`)
+                .url(`http://93.126.97.71:10080/api/pet`)
                 .method('PUT')
                 .body(pet)
                 .send<operations['updatePet']['responses']['200']['schema']>()
@@ -51,7 +102,7 @@ export class  PetController {
     async delete(id: number | string) {
         return (
             await new JsonRequest()
-                .url(`http://localhost/v2/pet/${id}`)
+                .url(`http://93.126.97.71:10080/api/pet/${id}`)
                 .method('DELETE')
                 .send<definitions['AbstractApiResponse']>()
         ).body
